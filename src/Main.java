5132b7925ab5e25644dc6fe86c1c4d378edcba3d
Pulkit Verma <pulkitverma23@gmail.com>
2019-05-02 12:47:22 -0700

    pulkit

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello Diego!");
+        System.out.println("Hello Pulkit!");
         System.out.println("Hello all. How are yall" );
     }
 }
