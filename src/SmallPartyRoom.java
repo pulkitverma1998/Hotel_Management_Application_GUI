9c692c27a4c5eb319563748e718dc7c5f4612669
Ajay Singh Bhawariya <asbhawariya@gmail.com>
2019-05-02 18:27:14 -0700

    Merge branch 'master' of /Users/asbhawariya/IdeaProjects/CECS_277_Final_Exam with conflicts.

--- src/SmallPartyRoom.java
+++ src/SmallPartyRoom.java
@@ -1,3 +1,18 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class SmallPartyRoom extends AbstractRoom{
 
+    private List<String> includedFeatures = new ArrayList<String>(Arrays.asList("Table & chair set-up","Basic Meal Plan"));
+
+    public SmallPartyRoom(){
+        super.setNumOfRooms(10);
+        super.setDescription("Room with party tables and chairs, adjacent to arcade");
+        super.setCapacity(30);
+        super.setCost(150);
+        super.setIncludedInCost(includedFeatures);
+        //upGrades
+
+    }
 }
