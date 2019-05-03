dfd1ee52d1db0c523acb2c38feb28e3d759116a8
Diego <diegocburela@gmail.com>
2019-05-02 18:43:12 -0700

    KaraokeLongeRoom class

--- src/KaraokeLoungeRoom.java
+++ src/KaraokeLoungeRoom.java
@@ -1,3 +1,14 @@
-public class KaraokeLoungeRoom extends AbstractRoom{
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
+public class KaraokeLoungeRoom extends AbstractRoom{
+    private List<String> included = new ArrayList<>(Arrays.asList("Add a meal Plan"));
+    public KaraokeLoungeRoom() {
+        setNumOfRooms(10);
+        setDescription("Enclosed longue with karaoke machine");
+        setCapacity(10);
+        setCost(30);
+        setIncludedInCost(included);
+    }
 }
