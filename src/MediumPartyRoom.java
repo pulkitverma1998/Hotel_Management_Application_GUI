79068dcd2a88b2166423cc850d49b56863d09627
Pulkit Verma <pulkitverma23@gmail.com>
2019-05-02 18:43:07 -0700

    medium party room created

--- src/MediumPartyRoom.java
+++ src/MediumPartyRoom.java
@@ -1,3 +1,17 @@
-public class MediumPartyRoom extends AbstractRoom{
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MediumPartyRoom extends AbstractRoom {
+
+    private List<String> includedInCost = new ArrayList<>(Arrays.asList("Table & Chairs set-up", "DJ", "Basic Meal Plan"));
+
+    public MediumPartyRoom() {
+        super.setNumOfRooms(2);
+        super.setDescription("Room with party tables and chairs, adjacent to arcade");
+        super.setCapacity(30);
+        super.setCost(150);
+        super.setIncludedInCost(includedInCost);
+    }
 
 }
