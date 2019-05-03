c913fbbff92419279d20a2139d92d879d2df04c7
Pulkit Verma <pulkitverma23@gmail.com>
2019-05-02 18:31:39 -0700

    change

--- src/AquaWorldRoom.java
+++ src/AquaWorldRoom.java
@@ -1,4 +1,20 @@
-public class AquaWorldRoom extends AbstractRoom{
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class AquaWorldRoom extends AbstractRoom {
+
+    private List<String> includedInCost = new ArrayList<>(Arrays.asList("access to showers/lockers", "life guards on duty", "DJ", "table & chair set-up, Basic Meal Plan"));
+
+    public AquaWorldRoom() {
+        super.setNumOfRooms(1);
+        super.setDescription("Olympic-sized pool with water slide, kiddie pool, and large jacuzzi");
+        super.setCapacity(75);
+        super.setCost(700);
+        super.setIncludedInCost(includedInCost);
+        super.setRestriction("To access water facilities, bathing suits must be worn at all times.");
+    }
+
 
 
 }
