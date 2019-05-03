a5e37629e03d441eb2856e69ae6973f1646f0fbc
richy <richy_riched@yahoo.com>
2019-05-03 09:20:17 -0700

    testing to see if my latest stuff will get pushed

---
+++ src/MealPlan.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+
+public class MealPlan {
+    private ArrayList<Pizza> pizzas ;
+    private ArrayList<String> sodaFlavors;
+    private String sodaName ;
+
+    public MealPlan(){
+        sodaName = "" ;
+    }
+
+    public void getUpgrades() {
+
+    }
+
+    public double getCost(){
+        double totalCost = 0 ;
+
+        return totalCost ;
+    }
+
+    public String getSodaName(){
+        return sodaName ;
+    }
+
+    public void setSodaName( String n ) {
+        sodaName = n;
+    }
+}
---
+++ src/Pizza.java
@@ -0,0 +1,6 @@
+
+public class Pizza {
+    //instance variables
+
+
+}
