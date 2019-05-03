930bda5778e1873fa8345c6cfb70f8c12effd9d9
Diego <diegocburela@gmail.com>
2019-05-02 18:31:54 -0700

    AdultBillards fix

--- src/AdultBilliardsLoungeRoom.java
+++ src/AdultBilliardsLoungeRoom.java
@@ -2,13 +2,15 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Arrays;
 
-public class AdultBilliardsLoungeRoom extends AbstractRoom{
+public class AdultBilliardsLoungeRoom extends AbstractRoom {
     private List<String> included = new ArrayList<>(Arrays.asList("Access to pool table and cues"));
-    public AdultBilliardsLoungeRoom(){
+
+    public AdultBilliardsLoungeRoom() {
         super.setNumOfRooms(5);
         super.setDescription("Enclosed longe with pool table");
         super.setCapacity(10);
         super.setCost(25);
         super.setIncludedInCost(included);
         super.setRestriction("21+ only");
+    }
 }
