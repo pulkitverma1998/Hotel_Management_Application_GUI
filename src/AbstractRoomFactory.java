c2ae93c7a808f03b87e6f19259d4919afad34702
Pulkit Verma <pulkitverma23@gmail.com>
2019-05-03 19:26:53 -0700

    change

--- src/AbstractRoomFactory.java
+++ src/AbstractRoomFactory.java
@@ -1,13 +1,5 @@
 public abstract class AbstractRoomFactory {
 
-public abstract AquaWorldRoom createAquaWorldRoom();
-
-public abstract SmallPartyRoom createSmallPartyRoom();
-
-public abstract  MediumPartyRoom createMediumPartyRoom();
-
-public abstract KaraokeLoungeRoom createKaraokeLoungeRoom();
-
-public abstract AdultBilliardsLoungeRoom createAdultBilliardsRoom();
+public abstract AbstractRoom createAbstractRoom();
 
 }
--- src/ConcreteRoomFactory.java
+++ src/ConcreteRoomFactory.java
@@ -1,32 +1,7 @@
 public class ConcreteRoomFactory extends AbstractRoomFactory {
 
     @Override
-    public AquaWorldRoom createAquaWorldRoom() {
-        AquaWorldRoom aquaWorldRoom = new AquaWorldRoom();
-        return aquaWorldRoom;
-    }
-
-    @Override
-    public SmallPartyRoom createSmallPartyRoom() {
-        SmallPartyRoom smallPartyRoom = new SmallPartyRoom();
-        return smallPartyRoom;
-    }
-
-    @Override
-    public MediumPartyRoom createMediumPartyRoom() {
-        MediumPartyRoom mediumPartyRoom = new MediumPartyRoom();
-        return mediumPartyRoom;
-    }
-
-    @Override
-    public KaraokeLoungeRoom createKaraokeLoungeRoom() {
-        KaraokeLoungeRoom karaokeLoungeRoom = new KaraokeLoungeRoom();
-        return karaokeLoungeRoom;
-    }
-
-    @Override
-    public AdultBilliardsLoungeRoom createAdultBilliardsRoom() {
-        AdultBilliardsLoungeRoom adultBilliardsLoungeRoom = new AdultBilliardsLoungeRoom();
-        return adultBilliardsLoungeRoom;
+    public AbstractRoom createAbstractRoom() {
+        return new AquaWorldRoom();
     }
 }
