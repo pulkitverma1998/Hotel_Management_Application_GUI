938d98b21ec6ae04143fec46e9a57d51f314cc1e
Ajay Singh Bhawariya <asbhawariya@gmail.com>
2019-05-04 17:13:36 -0700

    making the reservation

--- src/Reservation.java
+++ src/Reservation.java
@@ -8,7 +8,67 @@ public class Reservation {
     String time;
     AbstractRoom reservedRoom;
 
-    ArrayList<AbstractRoom> myRooms = ConcreteRoomFactory.allAvailableRooms;
+    //ArrayList<AbstractRoom> myRooms = ConcreteRoomFactory.allAvailableRooms;
+
+    public Reservation(){
+        mealPlan= "Basic Meal Plan";
+        //TODO upgrades
+        date = null;
+        time= null;
+        reservedRoom=null;
+        specialInstructions=null;
+    }
+
+//
+//    public Reservation(String mealPlan, String specialInstructions, String date, String time, AbstractRoom reservedRoom) {
+//        this.mealPlan = mealPlan;
+//        this.specialInstructions = specialInstructions;
+//        this.date = date;
+//        this.time = time;
+//        this.reservedRoom = reservedRoom;
+//        //TODO upgrades
+//    }
+
+    public String getMealPlan() {
+        return mealPlan;
+    }
+
+    public void setMealPlan(String mealPlan) {
+        this.mealPlan = mealPlan;
+    }
+
+    public String getSpecialInstructions() {
+        return specialInstructions;
+    }
+
+    public void setSpecialInstructions(String specialInstructions) {
+        this.specialInstructions = specialInstructions;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public AbstractRoom getReservedRoom() {
+        return reservedRoom;
+    }
+
+    public void setReservedRoom(AbstractRoom reservedRoom) {
+        this.reservedRoom = reservedRoom;
+    }
+
     public void update(){
     }
 
