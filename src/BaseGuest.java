fbe539ec6cee7f2ce092bd32bec800436d325c52
Diego Burela <diegocburela@gmail.com>
2019-05-07 18:43:30 -0700

    Diego (#10)
    * created factories class
    
    * Room Upgrades classes created
    
    * We can create a kaorke room and test harness updated
    
    * created factories class

---
+++ src/BaseGuest.java
@@ -0,0 +1,3 @@
+public interface BaseGuest {
+    public void update();
+}
--- src/Guest.java
+++ src/Guest.java
@@ -1,16 +1,68 @@
-public class Guest {
-    String name;
-    String phoneNum;
-    String email;
-    String creditCard;
+import java.util.ArrayList;
+
+public class Guest implements BaseGuest{
+    private String name;
+    private String phoneNum;
+    private String email;
+    private String creditCard;
+    private String date;
+    private String time;
+    public ArrayList<RoomUpdrages> upgrades = new ArrayList<>();
     public Guest(String name, String phoneNum, String email, String creditCard){
         this.name = name;
         this.phoneNum = phoneNum;
         this.email = email;
         this.creditCard = creditCard;
     }
-
     public void update() {
         System.out.println("Your room Available! Confirm your spot by paying 25% of the fee: ");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNum() {
+        return phoneNum;
+    }
+
+    public void setPhoneNum(String phoneNum) {
+        this.phoneNum = phoneNum;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCreditCard() {
+        return creditCard;
+    }
+
+    public void setCreditCard(String creditCard) {
+        this.creditCard = creditCard;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
     }
 }
